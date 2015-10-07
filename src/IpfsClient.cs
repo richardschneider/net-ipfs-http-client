86311d90b283cc2b0c492febba7059ea7c1eae4b
Richard Schneider <makaretu@gmail.com>
2015-10-07 17:29:42 +1300

    Add the solution infrastructure.

---
+++ src/IpfsClient.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Ipfs.Api
+{
+    /// <summary>
+    ///   A client that allows access to the InterPlanetary File System (IPFS).
+    /// </summary>
+    public class IpfsClient
+    {
+    }
+}
---
+++ test/IpfsClientTest.cs
@@ -0,0 +1,27 @@
+﻿using Ipfs.Api;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace Ipfs.Api
+{
+    
+    
+    /// <summary>
+    ///This is a test class for IpfsClientTest and is intended
+    ///to contain all IpfsClientTest Unit Tests
+    ///</summary>
+    [TestClass]
+    public class IpfsClientTest
+    {
+
+        /// <summary>
+        ///   A test for IpfsClient Constructor
+        ///</summary>
+        [TestMethod]
+        public void Can_Create()
+        {
+            IpfsClient target = new IpfsClient();
+            Assert.IsNotNull(target);
+        }
+    }
+}
