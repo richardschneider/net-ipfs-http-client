aa2638452a614d6f38bcd96260bfc4f97a6ac269
Richard Schneider <makaretu@gmail.com>
2016-12-23 11:22:49 +1300

    feat(generic): implement IPFS Generic API

---
+++ src/Commands/GenericApi.cs
@@ -0,0 +1,37 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+using System.Net;
+using System.Threading.Tasks;
+
+namespace Ipfs.Api
+{
+    public partial class IpfsClient
+    {
+        /// <summary>
+        ///   Information about an IPFS peer.
+        /// </summary>
+        /// <param name="peer">
+        ///   The id of the IPFS peer.  If not specified (e.g. null), then the local
+        ///   peer is used.
+        /// </param>
+        /// <returns>
+        /// </returns>
+        public Task<PeerNode> Id(string peer = null)
+        {
+            return DoCommandAsync<PeerNode>("id", peer);
+        }
+
+        /// <summary>
+        ///   Get the version information of the API server.
+        /// </summary>
+        public Task<Dictionary<string, string>> Version()
+        {
+            return DoCommandAsync<Dictionary<string, string>>("version");
+        }
+
+    }
+}
---
+++ test/Commands/GenericTest.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Newtonsoft.Json.Linq;
+using System;
+using System.Text;
+
+namespace Ipfs.Api
+{
+    [TestClass]
+    public class GenericTest
+    {
+        const string marsId = "QmSoLMeWqB7YGVLJN3pNLQpmmEk35v6wYtsMGLzSr5QBU3";
+
+        [TestMethod]
+        public void Local_Node_Info()
+        {
+            var ipfs = new IpfsClient();
+            var node = ipfs.Id().Result;
+            Assert.IsInstanceOfType(node, typeof(PeerNode));
+        }
+
+        [TestMethod]
+        public void Mars_Node_Info()
+        {
+            var ipfs = new IpfsClient();
+            var node = ipfs.Id(marsId).Result;
+            Assert.IsInstanceOfType(node, typeof(PeerNode));
+        }
+
+        [TestMethod]
+        public void Version_Info()
+        {
+            var ipfs = new IpfsClient();
+            var versions = ipfs.Version().Result;
+            Assert.IsNotNull(versions);
+            Assert.IsTrue(versions.ContainsKey("Version"));
+            Assert.IsTrue(versions.ContainsKey("Repo"));
+        }
+
+    }
+}
+
