fc3b41efe9df702efd42815faa8716603bf6d519
Richard Schneider <makaretu@gmail.com>
2017-01-14 22:06:21 +1300

    test(PinnedObject): some tests

---
+++ test/PinnedObjectTest.cs
@@ -0,0 +1,25 @@
+﻿using Ipfs.Api;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Linq;
+
+namespace Ipfs.Api
+{
+
+    [TestClass]
+    public class PinnedObjectTest
+    {
+
+        [TestMethod]
+        public void Stringify()
+        {
+            var pin = new PinnedObject
+            {
+                Id = "QmStfpa7ppKPSsdnazBy3Q5QH4zNzGLcpWV88otjVSV7SY",
+                Mode = PinMode.Recursive
+            };
+            Assert.AreEqual("QmStfpa7ppKPSsdnazBy3Q5QH4zNzGLcpWV88otjVSV7SY Recursive", pin.ToString());
+        }
+
+    }
+}
