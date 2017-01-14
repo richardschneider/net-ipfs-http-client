9f93ecba87c9edf024f82f393f73ad2bf49608cf
Richard Schneider <makaretu@gmail.com>
2017-01-14 22:20:42 +1300

    test(PinnedObjects): oops

---
+++ test/PinnedObjectsTest.cs
@@ -0,0 +1,36 @@
+﻿﻿using Ipfs.Api;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Linq;
+
+namespace Ipfs.Api
+{
+
+    public partial class IpfsClientTest
+    {
+
+        [TestMethod]
+        public void Pin_List()
+        {
+            var ipfs = new IpfsClient();
+            Assert.IsNotNull(ipfs.PinnedObjects);
+            Assert.IsTrue(ipfs.PinnedObjects.Count > 0);
+        }
+
+        [TestMethod]
+        public void Pin_Add_Remove()
+        {
+            var ipfs = new IpfsClient();
+            var result = ipfs.AddTextAsync("I am pinned").Result;
+            var id = result.Hash;
+
+            ipfs.PinnedObjects.Add(id);
+            Assert.IsTrue(ipfs.PinnedObjects.Contains(id));
+
+            ipfs.PinnedObjects.Remove(id);
+            Assert.IsFalse(ipfs.PinnedObjects.Contains(id));
+        }
+
+
+    }
+}
