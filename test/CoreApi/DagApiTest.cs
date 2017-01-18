2344db78f499b1beb5e39a8aa47cf30e829264d6
Richard Schneider <makaretu@gmail.com>
2017-01-18 15:34:21 +1300

    test(DagApi): is not implemented

---
+++ test/CoreApi/DagApiTest.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Newtonsoft.Json.Linq;
+using System;
+using System.Text;
+
+namespace Ipfs.Api
+{
+    [TestClass]
+    public class DagApiTest
+    {
+        [TestMethod]
+        public void GetAsync()
+        {
+            var ipfs = TestFixture.Ipfs;
+            ExceptionAssert.Throws<NotImplementedException>(() => ipfs.Dag.GetAsync("cid"));
+        }
+
+        [TestMethod]
+        public void PutAsync()
+        {
+            var ipfs = TestFixture.Ipfs;
+            ExceptionAssert.Throws<NotImplementedException>(() => ipfs.Dag.PutAsync(null, null));
+        }
+
+    }
+}
+
