f13699367241827b73c793ba04ca07f4389c5dc8
Richard Schneider <makaretu@gmail.com>
2017-01-18 15:05:05 +1300

    test(Block): add tests for a Block

---
+++ test/BlockTest.cs
@@ -0,0 +1,39 @@
+﻿using Ipfs.Api;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Linq;
+
+namespace Ipfs.Api
+{
+
+    [TestClass]
+    public class DataBlockTest
+    {
+        byte[] someBytes = new byte[] { 1, 2, 3 };
+
+        [TestMethod]
+        public void DataBytes()
+        {
+            var block = new Block
+            {
+                DataBytes = someBytes
+            };
+            CollectionAssert.AreEqual(someBytes, block.DataBytes);
+        }
+
+        [TestMethod]
+        public void DataStream()
+        {
+            var block = new Block
+            {
+                DataBytes = someBytes
+            };
+            var stream = block.DataStream;
+            Assert.AreEqual(1, stream.ReadByte());
+            Assert.AreEqual(2, stream.ReadByte());
+            Assert.AreEqual(3, stream.ReadByte());
+            Assert.AreEqual(-1, stream.ReadByte(), "at eof");
+        }
+
+    }
+}
